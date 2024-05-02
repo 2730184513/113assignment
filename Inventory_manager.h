8adb5daa280654fac3dfa5f68db0de5ba76cb8dd
Deng Chujie <dengchujie@student.usm.my>
2024-05-02 13:57:52 +0800

    创建新的头文件

---
+++ Inventory_manager.h
@@ -0,0 +1,12 @@
+//
+// Created by 邓初杰 on 24-5-2.
+//
+
+#ifndef INVENTORY_MANAGER_H
+#define INVENTORY_MANAGER_H
+#include "Abstract_Product_class.h"
+#include<iostream>
+#include <string>
+using namespace std;
+
+#endif //INVENTORY_MANAGER_H
